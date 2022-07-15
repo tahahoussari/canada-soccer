e24fc71f526c2985f7e2c0eabb1d2589be9d32df
tahahoussari <tahahoussari@gmail.com>
2022-07-15 15:44:05 -0400

    Started coding

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Canada Soccer</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/">Home</a>
+                <a href="/">Home</a>
+                <a href="/">Home</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
