cafe76771c1e65b5fbf35f192be16db0a0959c61
tahahoussari <tahahoussari@gmail.com>
2022-07-18 14:09:20 -0400

    Added some more stuff

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,56 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from './teamphoto.jpeg';
+import React from "react";
+
+
 const Home = () => {
     return (
-        <div className="home">
-            <h2>Homepage</h2>
-        </div>
+      <Container>
+        <Row>
+          <Col className="home">
+          <h1 className="title">Follow Canada's journey through the World Cup here!</h1>
+          </Col>
+          <Col>
+          <img src={Image} className="teamPhoto"></img>
+          </Col>
+          <Col>
+          <p className="paragraph">Follow along as Canada has qualified for it's first world cup since 1986! 
+          This website was made so that Team Canada fans across the country can keep
+          up with all the latest scores and news surrounding the Canadian Soccer Team
+          as they make their way through the World Cup in Qatar.
+          </p>
+          </Col>
+
+          <Col>
+            <h4 className="titleMatch">Upcoming Matches</h4>
+                <ul className="matchList">
+                    <div>
+                        <h5 className="game">Canada vs. Belgium</h5>
+                    </div>
+                    <p className="date">nov 23, 2022</p>
+
+                    <div>
+                        <h5 className="game">Canada vs. Croatia</h5>
+                    </div>
+                    <p className="date">nov 23, 2022</p>
+
+                    <div>
+                        <h5 className="game">Canada vs. Morocco</h5>
+                    </div>
+                    <p className="date">nov 23, 2022</p>
+                </ul>
+          </Col>
+
+          <Col className="highlights">
+          <h4 className="titleMatch">Watch the previous game highlights</h4>
+          <div>
+          <iframe src="https://www.youtube.com/embed/sCkfsoJ_FII" height={500} width={900} allowfullscreen />
+          </div>
+          </Col>
+        </Row>
+      </Container>
     );
 }
  
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,31 @@
+import Container from 'react-bootstrap/container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 const Navbar = () => {
     return (
-        <nav className="navbar">
-            <h1>Canada Soccer</h1>
-            <div className="links">
-                <a href="/">Home</a>
-                <a href="/">Home</a>
-                <a href="/">Home</a>
-                <a href="/">Home</a>
-            </div>
-        </nav>
+        <div className="cont">
+            <nav className="navbar">
+                <Container>
+                    <Col>
+                        <div className="logo">
+                            <Row>
+                                <h1>Canada Soccer</h1>
+                            </Row>
+                        </div>
+                        <div className="links">
+                            <Row>
+                                <a href="/">Team</a>
+                                <a href="/">Schedule</a>
+                                <a href="/">Stats</a>
+                                <a href="/create">Quizzes</a>
+                            </Row>
+                        </div>
+                    </Col>
+                </Container>
+            </nav>
+        </div>
+        
     );
 }
  
